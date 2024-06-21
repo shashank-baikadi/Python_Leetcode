a9d811d369431fbef115fc0d3c2fcabfa33b6a37
ShashankBaikadi1 <baikadi.shashank@infokalash.com>
2024-06-21 14:46:40 +0530

    Refactor List.py to fix typo in variable name

--- List.py
+++ List.py
@@ -20,5 +20,5 @@ type(l1)
 #tuple-(),set-{},list-[],dict-{key:value},str-"",int-10,float-10.5,bool-True/False,complex-10+20j
 print(type(l1))
 
-l2=[40,True,'shashank',10.5,10+20j] #list can store any type of data-hetrogenous
+l2=[40,True,'shashank-',10.5,10+20j] #list can store any type of data-hetrogenous
 print(l2[0] ,l1[2] ,l2[-1])# we can access the element of the list by using index in list
