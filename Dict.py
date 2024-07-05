509a354cfddb4987118589e69d7a025ab08598fd
shashank-baikadi <shashankbaikadi1@gmail.com>
2024-07-06 00:33:26 +0530

    dict file created

---
+++ Dict.py
@@ -0,0 +1,11 @@
+'''
+dict is a class
+its mutable
+not a sequence
+is iterable
+indexing is not pplicable to dict object
+dict element are pai of key-value and data-value
+slicing operator is not applicatbel
+
+'''
+
