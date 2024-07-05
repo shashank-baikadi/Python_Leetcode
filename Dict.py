045b9473945f7ec94e48e3f0c0a889d46159a367
shashank-baikadi <shashankbaikadi1@gmail.com>
2024-07-06 00:36:36 +0530

    Refactor Dict.py for improved readability and adherence to coding conventions

--- Dict.py
+++ Dict.py
@@ -7,5 +7,7 @@ indexing is not pplicable to dict object
 dict element are pai of key-value and data-value
 slicing operator is not applicatbel
 
+
+in side set all value are key which cant be duplicate where as in dict we have key-data pair
 '''
 
