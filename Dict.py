7a4b3186842182a314ea5e6e9ee3649ac6c4e8e2
shashank-baikadi <shashankbaikadi1@gmail.com>
2024-07-06 00:44:36 +0530

    Refactor Dict.py for improved readability and adherence to coding conventions

--- Dict.py
+++ Dict.py
@@ -10,4 +10,18 @@ slicing operator is not applicatbel
 
 in side set all value are key which cant be duplicate where as in dict we have key-data pair
 '''
+d1={1:'one',2:'two',3:'three'}
+print(d1)
+print(type(d1))
 
+#empty dict
+d2={}
+print(d2)   
+print(type(d2))
+
+
+#create a d3 dict object
+d3=dict()
+
+d4=dict(a='shashank',b='baikadi')
+print(d4)
