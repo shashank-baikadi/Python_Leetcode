8c4e9f6f4d7826d42dcc9d0df1bcd1914e4ef5be
shashank-baikadi <shashankbaikadi1@gmail.com>
2024-07-06 19:14:08 +0530

    Refactor Func.py for improved readability and adherence to coding conventions

---
+++ Func.py
@@ -0,0 +1,12 @@
+'''
+function is a block  of code which only runs when its called 
+
+def functionName(args):
+   code 
+   code 
+'''
+
+def shashank():
+    print("shashank")
+
+shashank()    # function call 
