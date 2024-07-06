597143e584a75a942231db32c246abd25b904712
shashank-baikadi <shashankbaikadi1@gmail.com>
2024-07-06 19:26:57 +0530

    Refactor Func.py for improved readability and adherence to coding conventions

--- Func.py
+++ Func.py
@@ -4,9 +4,22 @@ function is a block  of code which only runs when its called
 def functionName(args):
    code 
    code 
+
+takes nothing ,returns nothing-when u cont pass anything in arg then it will be this function only function call will be like this shashank() 
+
+variable-global and local variables
+
+def f1(args):
+    x=10-----local variable can be used only in the function
+
+y=10----global variable    
+
+
+
+
 '''
 
-def shashank():
-    print("shashank")
+def shashank(a,b):
+    print("shashank ",a,b)
 
-shashank()    # function call 
+shashank(10,20)    # function call 
