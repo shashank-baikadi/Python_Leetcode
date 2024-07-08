df71c59727095d7896be55cdbe69309dd6b0424b
shashank-baikadi <shashankbaikadi1@gmail.com>
2024-07-09 00:03:22 +0530

    Refactor recursion function for proper termination condition and improved readability

--- Recursion.py
+++ Recursion.py
@@ -17,3 +17,23 @@ def F1(n):
     return s
 x=F1(5)
 print(x)
+
+
+# write a recurion function to calulate sun of first n natural number
+def sum(n):
+    if n==1:
+        return 1
+    s=n+sum(n-1)
+    return s
+y=sum(10)
+print(y)
+
+
+# write a recurssive function to calculate sum of square of first n natural numbers
+def sumsquare(n):
+    if n==1:
+        return 1
+    s=n*n+sumsquare(n-1)
+    return s
+z=sumsquare(10)
+print(z)
