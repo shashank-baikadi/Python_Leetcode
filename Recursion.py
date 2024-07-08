bffd09e1688983ae36afa85448ab5248bd05a229
shashank-baikadi <shashankbaikadi1@gmail.com>
2024-07-08 23:48:20 +0530

    Refactor recursion function for proper termination condition and improved readability

--- Recursion.py
+++ Recursion.py
@@ -2,6 +2,18 @@
 
 def f():
     print('hi')
-    f()  
+    # f()  
     print('bye')
 f()
+
+#Recursion tree
+
+# how to dry run a code
+
+def F1(n):
+    if n==1:
+        return 1
+    s=n+F1(n-1)
+    return s
+x=F1(5)
+print(x)
