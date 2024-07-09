09ceb814abe04e94b781cbf2dda98962cf31f939
shashank-baikadi <shashankbaikadi1@gmail.com>
2024-07-09 22:49:59 +0530

    feat: Add recursive factorial calculation using lambda expression

--- Lambda.py
+++ Lambda.py
@@ -11,3 +11,17 @@ print(add(2,3))
 
 f1=(lambda a,b:a+b)(10,20)
 print(f1)
+
+#Recursion using Lambda
+#calucalate factorial of a number using recursion lambda expression 
+
+#normal
+def fact(n):
+    if n==0 or n==1:
+        return 1
+    
+    return n* fact(n-1)
+
+# Using LambdaExpression
+f=lambda n: 1 if n==1 or n==1 else n*f(n-1)
+print(f(5))
